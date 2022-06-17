ecdc922544f9d6161df16a04bda3ccfcb2f3567f
Anil Kumar <anilk.active@gmail.com>
2022-06-17 16:29:26 +0530

    req.txt n settings

--- flasher/settings.py
+++ flasher/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qbw$+=b1m&&g%vn^#wh24q_9!$p+(4asn%h_pcp!ci@4cuo@0f'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['flasher2-env.eba-2rzmbkhs.us-west-2.elasticbeanstalk.com']
 
