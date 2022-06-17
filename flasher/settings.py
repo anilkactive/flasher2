7a0cfae2173dbb4cda23fe478055006e1a539745
Anil Kumar <anilk.active@gmail.com>
2022-06-17 16:53:22 +0530

    settings

--- flasher/settings.py
+++ flasher/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qbw$+=b1m&&g%vn^#wh24q_9!$p+(4asn%h_pcp!ci@4cuo@0f'  #config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
