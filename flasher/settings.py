db485cc77d6195f77357cb144b7b3ccf5b989413
Anil Kumar <anilk.active@gmail.com>
2022-06-18 05:45:02 +0530

    settings

--- flasher/settings.py
+++ flasher/settings.py
@@ -155,8 +155,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 # STATIC_URL = '/static/'
-STATIC_URL = 'blinker-env.eba-ittep7em.us-west-2.elasticbeanstalk.com/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'flasher/static',
